7266c6aa9680f399955f2c45d16bafeef5417501
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2026-05-21 02:53:42 +0300

    add JS deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,14 @@ import * as github from '@actions/github'
 import * as exec from '@actions/exec'
 
 function run() {
+    // Get input values
+    let bucket = core.getInput('bucket', { required: true });
+    let bucketRegion = core.getInput('bucket-region', { required: true });
+    let distFolder = core.getInput('dist-folder', { required: true });
+    // Upload files
+    const s3Uri = `s3://${bucket}/${distFolder}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
     core.notice('Hello from my JavaScript Action!')
 }
 
